1a15d2628bed9de36d6814566b423a66f2ed26eb
KaranP3 <karanpillai93@gmail.com>
2021-12-01 03:32:57 +0530

    add length

--- src/main/scala/com/github/karanp3/lists/ListProblems.scala
+++ src/main/scala/com/github/karanp3/lists/ListProblems.scala
@@ -3,29 +3,43 @@ package com.github.karanp3.lists
 import scala.annotation.tailrec
 
 sealed abstract class RList[+T] {
+  /**
+   * Standard functions
+   */
   def head: T
   def tail: RList[T]
   def isEmpty: Boolean
-
   def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
 
+  /**
+   * Easy problems
+   */
+  // get element at a given index
   def apply(index: Int): T
+
+  // the size of the list
+  def length: Int
 }
 
 case object RNil extends RList[Nothing] {
   override def head: Nothing = throw new NoSuchElementException
   override def tail: RList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-
   override def toString: String = "[]"
 
+  /**
+   * Easy problems
+   */
+  // get element at a given index
   override def apply(index: Int): Nothing = throw new NoSuchElementException
+
+  // the size of the list
+  override def length: Int = 0;
 }
 
 case class ::[+T](override val head: T, override val tail: RList[T])
     extends RList[T] {
   override def isEmpty: Boolean = false
-
   override def toString: String = {
     @tailrec
     def toStringTailRec(remaining: RList[T], result: String): String = {
@@ -37,7 +51,11 @@ case class ::[+T](override val head: T, override val tail: RList[T])
     "[" + toStringTailRec(this, "") + "]"
   }
 
-  // get k-th element
+  /**
+   * Easy problems
+   */
+
+  // get element at a given index
   override def apply(index: Int): T = {
     @tailrec
     def applyTailRec(remaining: RList[T], currentIndex: Int): T = {
@@ -48,10 +66,26 @@ case class ::[+T](override val head: T, override val tail: RList[T])
     if (index < 0) throw new NoSuchElementException
     else applyTailRec(this, 0)
   }
+
+  // the size of the list
+  override def length: Int = {
+    @tailrec
+    def lengthTailRec(list: RList[T], acc: Int): Int = {
+      if (list.isEmpty) acc
+      else lengthTailRec(list.tail, acc + 1)
+    }
+
+    lengthTailRec(this, 0)
+  }
 }
 
 object ListProblems extends App {
   val aSmallList = 1 :: 2 :: 3 :: RNil
-  println(aSmallList)
-  println(aSmallList(2))
+
+  // test get k-th
+  println(aSmallList.apply(0))
+  println(aSmallList.apply(1))
+
+  // test length
+  println(aSmallList.length)
 }
